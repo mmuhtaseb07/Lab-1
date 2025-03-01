411faa77f9acf6c297e02eb4337cd0dfb59d7fa2
mmuhtaseb07 <mmuhtase@montgomerycollege.edu>
2025-02-28 19:02:59 -0500

    Add files via upload

---
+++ Recursion Lab/ArraySum.java
@@ -0,0 +1,14 @@
+package lab4;
+
+public class ArraySum {
+	
+	public int sumOfArray (Integer[] a,int index) {
+		
+		if (index == 0) {
+			return a[index];
+		}
+		
+		return a[index] + sumOfArray(a,index - 1);
+	}
+	
+}
